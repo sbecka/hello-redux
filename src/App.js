35eefb2175c5770aaa2c070583735a9975c90762
Sabrina Becker <sbecka8@gmail.com>
2020-06-01 17:07:29 -0400

    Create initial actions, reducers, and store folders and files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
+import reducer from './reducers';
+import { createStore } from 'redux';
+const initialState = { tech: "React " };
+const store = createStore(reducer, initialState); // reducer is the mandatory argument
+// reducer always talks to the store, store needs to be invoked with a reducer
+// like Array.reduce(), createStore() responsible for passing the args into the reducer
+// reducer has state and action arguments
+
 
 class App extends Component {
-  state = {
-    tech: "React"
-  };
+
   render() {
-    return <HelloWorld tech={this.state.tech} />;
+    return <HelloWorld tech={store.getState().tech} />;
   }
 }
 
